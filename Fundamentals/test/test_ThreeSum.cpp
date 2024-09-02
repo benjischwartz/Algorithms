1d1f04d4e9937c58e68abe68a2a84f663500255b
Benji Schwartz <benji.schwartz2013@gmail.com>
2024-09-02 12:37:28 +1000

    Add GTest and remove 4Sum

--- Fundamentals/test/test_ThreeSum.cpp
+++ Fundamentals/test/test_ThreeSum.cpp
@@ -2,11 +2,17 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
+void print_output(std::vector<std::vector<int>>& result);
+
 TEST(ThreeSumTest, BasicTest) {
     ThreeSum solver;
     std::vector<int> nums = {-4, -3, -2, -2, -1, 0, 0, 0, 1, 1, 2, 2, 3, 4, 5, 6};
+    std::vector<std::vector<int>> expected = 
+    {{-4, -2, 6}, {-4, -1, 5}, {-4, 0, 4}, {-4, 1, 3}, {-4, 2, 2}, 
+    {-3, -2, 5}, {-3, -1, 4}, {-3, 0, 3}, {-3, 1, 2}, {-2, -2, 4}, 
+    {-2, -1, 3}, {-2, 0, 2}, {-2, 1, 1}, {-1, 0, 1}, {0, 0, 0}};
     std::vector<std::vector<int>> result = solver.find3Sum(nums);
-
+    EXPECT_EQ(result, expected);
 }
 
 void print_output(std::vector<std::vector<int>>& result) {
